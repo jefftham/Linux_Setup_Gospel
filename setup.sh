b0997fa00330d2181ee53ce4a0352161f3c8ad34
Jeff Tham <Jeff.Tham@email.com>
2016-07-26 21:30:55 -0400

    added setup.sh to trigger all relevant scripts at once

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#this is the main setup script that trigger other all relevant scripts
+
+sudo apt-get update
+
+sudo apt-get upgrade -y
+
+#install Node Version Manager and NodeJS
+source node_setup.sh
